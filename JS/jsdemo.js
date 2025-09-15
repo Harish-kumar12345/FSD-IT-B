8cb4978ebf2b95452ee4181e863af3bd83d502c7
Harish Kumar <harishkumarup286@gmail.com>
2025-09-15 13:05:30 +0530

    created js and html file

---
+++ JS/jsdemo.js
@@ -0,0 +1,41 @@
+// let a=20;
+// console.log(a);
+
+// function hello(user='Guest'){
+//     console.log(`Hello ${user}`);
+// }
+// hello();
+// hello('Harish');
+
+function sum(a, b){
+    // console.log('Before retrun');
+    return a+b;
+    // console.log("after return");      //unreachable 
+}
+// console.log(sum(6,6));
+// console.log(sum());
+
+
+//*********Arrow function*********** */
+
+// const add=(a,b)=>{a+b};       //need to return {}
+// console.log(add(2,3));
+
+
+//IIFE
+
+// (function(){
+//     console.log("Hello Coders");
+// })();
+
+// CALLBACK 
+
+function hello(user, callback){
+    console.log(`hello ${user}`);
+    callback();
+}
+hello("admin",()=>
+console.log(sum(4,3)));
+
+hello("admin",function(){
+console.log(sum(4,8))});
