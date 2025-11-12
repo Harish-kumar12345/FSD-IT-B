2d3b17064ec6cc68cc12ba03c0a030f39e02d137
Harish Kumar <harishkumarup286@gmail.com>
2025-11-12 10:11:56 +0530

    Create A foler

---
+++ Shopping-app/shopping-app/src/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <div className='header'> {
+     
+    }
+      
+    </div>
+  )
+}
+
+export default AdminDashboard
---
+++ Shopping-app/shopping-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/register" element={<h1>Register Page</h1>} />
+        <Route path="*" element={<h1>404! Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
